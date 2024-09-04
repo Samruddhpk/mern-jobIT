27d610286a41ab14ed30d8ec198b9dca5d4c956e
Samruddh pk <107286956+Samruddhpk@users.noreply.github.com>
2024-09-04 15:22:59 +0530

    added: Search/ filter options to All Jobs Page

--- client/src/components/SearchContainer.jsx
+++ client/src/components/SearchContainer.jsx
@@ -1,6 +1,48 @@
+import { FormRow, FormRowSelect, SubmitBtn } from "../components";
+import Wrapper from "../assets/wrappers/DashboardFormPage";
+import { Form, useSubmit, Link } from "react-router-dom";
+import { JOB_TYPE, JOB_SORT_BY, JOB_STATUS } from "../../../utils/constants";
+import { useAllJobsContext } from "../pages/AllJobs";
+
 const SearchContainer = () => {
+    const { searchValues } = useAllJobsContext();
+    const { search, jobStatus, jobType, sort } = searchValues;
+
+    const debounce = (onChange) => {
+        let timeout;
+        return (e) => {
+            const form = e.currentTarget.form;
+            clearTimeout(timeout);
+            timeout = setTimeout(() => {
+                onChange(form);
+            }, 2000);
+        };
+    };
+
+    const submit = useSubmit();
     return (
-        <div>SearchContainer</div>
+        <Wrapper>
+            <Form className="form">
+                <h5 className="form-title">search form</h5>
+                <div className="form-center">
+                    <FormRow type="search" name="search" defaultValue="a" onChange={debounce((form) => {
+                        submit(form);
+                    })} />
+
+                    <FormRowSelect labelText="job status" name="jobStatus" list={["all", ...Object.values(JOB_STATUS)]} defaultValue="all" onChange={((e) => { submit(e.currentTarget.form); })} />
+
+                    <FormRowSelect labelText="job type" name="jobType" list={["all", ...Object.values(JOB_TYPE)]} onChange={((e) => { submit(e.currentTarget.form); })} />
+
+                    <FormRowSelect name="sort" defaultValue="newest" list={[...Object.values(JOB_SORT_BY)]} onChange={((e) => { submit(e.currentTarget.form); })} />
+
+                    <Link to="/dashboard/all-jobs" className="btn form-btn delete-btn">
+                        Reset Search Values
+                    </Link>
+                    {/* Temp */}
+                    <SubmitBtn formBtn />
+                </div>
+            </Form>
+        </Wrapper>
     );
 };
 export default SearchContainer;
